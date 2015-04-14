e0b895d7bd1b66171571d1fc37fd81d04a368aef
QuentinV <quentvht@hotmail.com>
2015-04-14 17:34:09 +0200

    prototype class tabou

---
+++ src/tabou/Tabou.java
@@ -0,0 +1,24 @@
+package tabou;
+
+import main.Solution;
+
+public class Tabou {
+
+	/**
+	 * Renvoi le meilleur voisin selon la fitness de la solution passée en paramètre
+	 * @param s : Solution de départ
+	 * @return Solution : meilleur voisin selon la fitness
+	 */
+	public Solution bestNeighbor(Solution s)
+	{
+		return null;
+	}
+	
+	/**
+	 * Main test algo tabou
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello how are you today ?");
+	}
+}
