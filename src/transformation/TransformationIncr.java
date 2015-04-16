0a5ff73536edc848b8e7b394cf44149b25b2bb4f
QuentinV <quentvht@hotmail.com>
2015-04-16 19:56:37 +0200

    TransformationIncr equals

--- src/transformation/TransformationIncr.java
+++ src/transformation/TransformationIncr.java
@@ -14,6 +14,12 @@ public class TransformationIncr implements Transformation {
 	private TypeImage tI;
 	private int idPattern;
 
+	/**
+	 * 
+	 * @param transformation
+	 * @param tI
+	 * @param idPattern
+	 */
 	public TransformationIncr(int transformation, TypeImage tI, int idPattern) {
 		super();
 		this.transformation = transformation;
@@ -21,22 +27,27 @@ public class TransformationIncr implements Transformation {
 		this.idPattern = idPattern;
 	}
 
-	public Solution transform (Solution s)
+	/**
+	 * Transforme la solution en paramètre avec cette transformation
+	 * @param s Solution à transformer
+	 * @return Solution transformée .. null
+	 */
+	public Solution transform(Solution s)
 	{
 		long timeStart = System.currentTimeMillis();
 		
-		Pattern p = s.getPatterns()[idPattern];
-		int nbTypeImage = p.getImgsNb().get(tI);
+		Pattern p = s.getPatterns()[idPattern]; //recuperer pattern
+		int nbTypeImage = p.getImgsNb().get(tI); //recuperer le nombre du type d'image correspondant dans le pattern
 
 		if ((transformation < 0 && nbTypeImage > 0) || 
 				(transformation > 0 && (((int) (Pattern.getSurface() / tI.getSurface())) > nbTypeImage + transformation)))
 		{ 
 			Map<TypeImage, Integer> newMapti = new HashMap<>(p.getImgsNb());
-			newMapti.replace(tI, nbTypeImage+transformation);
+			newMapti.replace(tI, nbTypeImage+transformation); //Modifier le type d'image pour le pattern selon la transformation
 			
 			Pattern newPattern = new Placement(newMapti).place();
 			if (newPattern != null) 
-			{
+			{ //Si le nouveau pattern créé est correct
 				Pattern[] newListPatterns = new Pattern[s.getPatterns().length];
 				for (int i=0; i<s.getPatterns().length; i++)
 				{
@@ -60,11 +71,11 @@ public class TransformationIncr implements Transformation {
 		this.transformation = transformation;
 	}
 
-	public TypeImage gettI() {
+	public TypeImage getTI() {
 		return tI;
 	}
 
-	public void settI(TypeImage tI) {
+	public void setTI(TypeImage tI) {
 		this.tI = tI;
 	}
 
@@ -76,4 +87,15 @@ public class TransformationIncr implements Transformation {
 		this.idPattern = idPattern;
 	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof TransformationIncr)
+		{
+			TransformationIncr t = (TransformationIncr) obj;
+			return this.transformation == t.getTransformation() && 
+					this.tI == t.getTI() && 
+					this.idPattern == t.getIdPattern();
+		} else 
+			return false;		
+	}
 }
